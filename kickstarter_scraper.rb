5db0db39de2aa443c84d88369d3482a27b116828
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 17:27:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,8 +18,9 @@ def create_project_hash
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project]
-  
+    projects[project] = {}
+  end
+
   binding.pry
 end
 
